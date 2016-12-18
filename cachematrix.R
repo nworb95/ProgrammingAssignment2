b6120c53c810622e6054d3a193bae661754502e0
nworb95 <nworb95@gmail.com>
2016-12-18 22:25:23 +0000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix creates a special matrix object, and initializes 'i' to be updated later
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL ## Initializes object i
+        set <- function(y) { ## Lets you change matrix later if you use $ subset
+                x <<- y ## Assigns your new value to the matrix
+                i <<- NULL ## Re-initializes object i 
+        }
+        get <- function() x ## Lets you retrieve matrix
+        setinverse <- function(inverse) i <<- inverse # Sets the inverse of the matrix
+        getinverse <- function() i ## Retrieves inverse of the matrix (?)
+        list(set=set, get=get, 
+             setinverse=setinverse, 
+             getinverse=getinverse) ## Names objects to allow use of $ operator
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve then solves the matrix (i.e. gives the inverse of the matrix), but then stores
+## it so that if cacheSolve is called again without changing the matrix, it is returned from the 
+## cache to save time as opposed to calculating it again
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        if(!is.null(i)){
+                message("perserverence is the answer")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinverse(i)
+        i
 }
